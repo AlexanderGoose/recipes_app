05de662729304568403f630a104ef298d5b54106
AlexanderGoose <alexandergoose97@gmail.com>
2024-05-03 13:36:48 -0600

    test data for api

---
+++ test_data.py
@@ -0,0 +1,27 @@
+data = {
+    "recipes": [
+        # ----------- recipe 1, pasta
+        {
+            'recID': 1,
+            'recName': 'Pasta',
+            'recIngredients': ['noodles', 'sauce', 'cheese'],
+            'recInstructions': ['boil pasta', 'heat sauce', 'sprinkle cheese on top']
+        },
+
+        # ----------- recipe 2, burgers
+        {
+            'recID': 2,
+            'recName': 'Burgers',
+            'recIngredients': ['testing'],
+            'recInstructions': ['testing']
+        },
+
+        # ----------- recipe 3, grilled cheese
+        {
+            'recID': 3,
+            'recName': 'Grilled Cheese',
+            'recIngredients': ['testing'],
+            'recInstructions': ['testing']
+        }
+    ]
+}
