78b8d1d688d9ffd541965a9a0c0426ef44466110
AlexanderGoose <alexandergoose97@gmail.com>
2024-05-04 14:07:57 -0600

    modified routes

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from test_data import data
 import sqlite3
+from dbAPI import DataBaseAPI
 
 app = Flask(__name__)
+
 
 
 # -------------------------------------------- home page
@@ -12,7 +14,12 @@ User will then navigate via navbar to where they want to go
 """
 @app.route('/home')
 def home():
+    # if DB doesn't exist, create it!
+    dbAPI = DataBaseAPI()
+    dbName = 'recipe.db'
+    dbAPI.create(dbName)
     return render_template('index.html')
+
 
 
 # -------------------------------------------- view ALL recipes (by name)
@@ -21,17 +28,40 @@ This route will simply show a list of all recipes that the user
 has access to. When a recipe is clicked, it will take to viewRec
 and fill in the <dbName> and <int:recID>
 """
-@app.route('/recipes')
-def recipes():
+@app.route('/recipes/<dbName>', methods=['GET', 'POST'])
+def recipes(dbName):
+    
+    # teardown
+    if request.method == 'POST':
+        dbAPI = DataBaseAPI()
+        # delete all tables to get rid of the db
+        dbAPI.teardown(dbName)
+
+        # recreate tables, now should be empty
+        # dbAPI.create(dbName)
+        return redirect(url_for('home'))
     
     ######## TO DO ########
     # impliment SQL to pull all recipe names and their ID's from db
     # within html file, for loop over it all
 
-    recipe_names = None
-    recIDs = None
+    conn = sqlite3.connect(dbName)
+    cur = conn.cursor()
 
-    return render_template('recipes.html', recipe_names=recipe_names, recIDs=recIDs)
+    # grab all recipe names
+    cur.execute("SELECT recName, recID FROM Recipe")
+    recipes = cur.fetchall()
+    # recipe_names = [row[0] for row in cur.fetchall()]
+    # recIDs = [row[1] for row in cur.fetchall()]
+
+    conn.close()
+
+    recipe_names = [recipe[0] for recipe in recipes]
+    recIDs = [recipe[1] for recipe in recipes]
+
+    return render_template('recipes.html', recipe_names=recipe_names,
+                           recIDs=recIDs)
+
 
 
 # -------------------------------------------- view recipe
@@ -63,15 +93,44 @@ def viewRec(dbName, recID):
                            ingredients=ingredients, instructions=instructions)
 
 
+
 # -------------------------------------------- add recipe
 """
 Will have a form to add new recipe. Function must be able to 
 take in the info, add to the db (using dbAPI.py ?????). Once
 complete, wipe the page and say complete.
 """
-@app.route('/add_recipe', methods=['POST'])
+@app.route('/add_recipe', methods=['GET', 'POST'])
 def add_recipe():
-    pass
+
+    # when form data is sent, 
+    if request.method == 'POST':
+        recipe_name = request.form.get('new_recipe_name')
+
+        ingredients = request.form.getlist('ingredient')
+        ingredients = [ing for ing in ingredients if ing.strip()] # filter out empty entries
+
+        instructions = request.form.getlist('instruction')
+        instructions = [inst for inst in instructions if inst.strip()] # filter out empty entries
+
+        rec_id = 100
+        dbName = 'recipe.db'
+
+        # creating an instance of the class
+        dbAPI = DataBaseAPI()
+        dbAPI.fill(dbName, recipe_name, ingredients, instructions)
+
+        return render_template('viewRecipes.html', recipe_name=recipe_name, 
+                               ingredients=ingredients, instructions=instructions,
+                               rec_id=rec_id)
+
+    return render_template('addRecipe.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 # -------------------------------------------- RUNS APP
