3155c28981512e93892ecb175b894b93b57d79f3
AlexanderGoose <alexandergoose97@gmail.com>
2024-05-13 13:02:24 -0600

    new routes for editing

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
 from test_data import data
-import sqlite3
 import psycopg2
 from dbAPI import DataBaseAPI
 
@@ -36,27 +35,12 @@ and fill in the <dbName> and <int:recID>
 # def recipes(dbName):
 @app.route('/recipes', methods=['GET', 'POST'])
 def recipes():
-    
-    # teardown
-    # if request.method == 'POST':
-    #     dbAPI = DataBaseAPI()
-    #     # delete all tables to get rid of the db
-    #     dbAPI.teardown(dbName)
-
-    #     # recreate tables, now should be empty
-    #     # dbAPI.create(dbName)
-    #     return redirect(url_for('home'))
 
     # here we grab the recipe names
     dbAPI = DataBaseAPI()
     #recipes = dbAPI.fetchRecipeNames(dbName)
     recipes = dbAPI.fetchRecipeNames()
 
-    recipe_names = [recipe[0] for recipe in recipes]
-    recIDs = [recipe[1] for recipe in recipes]
-
-    # return render_template('recipes.html', recipe_names=recipe_names,
-    #                        recIDs=recIDs)
     return render_template('recipes.html', recipes=recipes)
 
 
@@ -66,7 +50,7 @@ Routes to ONE recipe at a time. The URL includes the dbName as well
 as the recID (int). Current example: http://127.0.0.1:5000/viewRec/test.db/1
 """
 #@app.route('/viewRec/<dbName>/<int:recID>', methods=['GET'])
-@app.route('/viewRec/<int:recID>', methods=['GET'])
+@app.route('/viewRec/<int:recID>', methods=['GET', 'POST'])
 def viewRec(recID):
 
     dbAPI = DataBaseAPI()
@@ -76,8 +60,17 @@ def viewRec(recID):
         return "Recipe not found :(", 404
 
     return render_template('viewRecipes.html', recipe_name=recipe_name, 
-                           ingredients=ingredients, instructions=instructions)
+                           ingredients=ingredients, instructions=instructions, recID=recID)
 
+
+@app.route('/beginEditRecipe/<int:recID>', methods=['POST'])
+def beginEditRecipe(recID):
+    if request.method == 'POST':
+        dbAPI = DataBaseAPI()
+        recipe_name, ingredients, instructions = dbAPI.getFullRecipe(recID)
+        return render_template('editRecipe.html', recID=recID, recipe_name=recipe_name,
+                               ingredients=ingredients, instructions=instructions)
+    
 
 
 # -------------------------------------------- add recipe
@@ -99,26 +92,51 @@ def add_recipe():
         instructions = request.form.getlist('instruction')
         instructions = [inst for inst in instructions if inst.strip()] # filter out empty entries
 
-        rec_id = 100
-        #dbName = 'recipe.db'
-
         # creating an instance of the class
         dbAPI = DataBaseAPI()
         #dbAPI.fill(dbName, recipe_name, ingredients, instructions)
         dbAPI.fill(recipe_name, ingredients, instructions)
         
         return render_template('viewRecipes.html', recipe_name=recipe_name, 
-                               ingredients=ingredients, instructions=instructions,
-                               rec_id=rec_id)
+                               ingredients=ingredients, instructions=instructions)
+                            #    rec_id=rec_id)
 
     return render_template('addRecipe.html')
+
+
+
+
+@app.route('/editRecipe/<int:recID>', methods=['GET', 'POST'])
+def editRecipe(recID):
+    if request.method == 'POST':
+
+        dbAPI = DataBaseAPI()
+
+        original_recipe_name = request.form.get('original_recipe_name')
+        recID = request.form.get('recID')
+
+        recipe_name = request.form.get('edit_recipe_name')
+
+        ingredients = request.form.getlist('ingredient')
+        ingredients = [ing for ing in ingredients if ing.strip()] # filter out empty entries
+
+        instructions = request.form.getlist('instruction')
+        instructions = [inst for inst in instructions if inst.strip()] # filter out empty entries
+
+        dbAPI.editRecipe(original_recipe_name, recipe_name, ingredients, instructions)
+
+        # pull new info after edit
+        recipe_name, ingredients, instructions = dbAPI.getFullRecipe(recID)
+        # render new recipe details
+        return render_template('viewRecipes.html', recipe_name=recipe_name,
+                               ingredients=ingredients, instructions=instructions, recID=recID)
+        # return "<h1>Edited</h1>"
+    # return "nope"
 
 
 @app.route('/about')
 def about():
     return render_template('about.html')
-
-
 
 # -------------------------------------------- RUNS APP
 if __name__ == '__main__':
